c12d72eb12262b7273a42aefae0b46c67bd3be07
Peter C. de Tagyos <peter@ptss.net>
2011-12-09 17:11:04 -0800

    Initial Commit

---
+++ CoolUIViewAnimations/AppDelegate.h
@@ -0,0 +1,19 @@
+//
+//  AppDelegate.h
+//  CoolUIViewAnimations
+//
+//  Created by Peter de Tagyos on 12/9/11.
+//  Copyright (c) 2011 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@class ViewController;
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@property (strong, nonatomic) ViewController *viewController;
+
+@end
---
+++ CoolUIViewAnimations/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  CoolUIViewAnimations
+//
+//  Created by Peter de Tagyos on 12/9/11.
+//  Copyright (c) 2011 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
